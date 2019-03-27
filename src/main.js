c172372d7227c4cc3fe07753fa944006ca822657
postzll <945717051@qq.com>
2019-03-27 14:55:31 +0800

    购物车组件完成

--- src/main.js
+++ src/main.js
@@ -7,15 +7,21 @@ import VueRouter from 'vue-router'
 Vue.use(VueRouter)
 // 导入组件
 import index from './components/index.vue'
+import cart from './components/cart.vue'
 // 规则
 let routes=[
   {
     path:'/',
     component:index
-  },{
+  },
+  {
     path:'/index',
     component:index
-  }
+  },
+  {
+    path:'/cart',
+    component:cart
+  },
 ]
 let router=new VueRouter({
   routes
